8a27c8a7d7aa064649c62db7c886d61eb27c9cf9
Lulz07 <masterluffy2007@gmail.com>
2022-05-20 01:08:21 +0800

    Add solution for exercise 1-3

---
+++ chapter-1/1-3/heading.c
@@ -0,0 +1,35 @@
+/* Exercise 1-3. (page 13)
+ *
+ * Modify the temperature conversion program to print a
+ * heading above the table.
+ *
+ * Written by Lulz07.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    float fahr, celsius;
+    int start_at, stop_at, step_size;
+
+    start_at = 0;
+    stop_at = 300;
+    step_size = 20;
+
+    printf(" Fahrenheit          Celsius\n");
+    printf(" ----------          -------\n");
+
+    fahr = start_at;
+    while (fahr <= stop_at) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        /* fahrenheit will be printed 7 characters wide
+         * meanwhile celsius is 19 characters wide and 1
+         * character after decimal point
+        */
+        printf("%7.0f %19.1f\n", fahr, celsius);
+        fahr = fahr + step_size;
+    }
+
+    return 0;
+}
