f9282479f7c57c38b66bd6b734a5a503391fe44a
Lulz07 <masterluffy2007@gmail.com>
2022-05-24 22:37:42 +0800

    Add solution for exercise 1-9

---
+++ chapter-1/1-9/replace-blanks.c
@@ -0,0 +1,34 @@
+/* Exercise 1-9. (page 20)
+ *
+ * Write a program to copy its input to its output, replacing
+ * each string of one or more blanks by a single blank.
+ *
+ * Written by Lulz07.
+*/
+
+#include <stdio.h>
+
+#define PREVIOUS_IS_A_BLANK 1
+#define PREVIOUS_IS_NOT_A_BLANK 0
+
+int main(void)
+{
+    int c, character_status;
+
+    character_status = PREVIOUS_IS_NOT_A_BLANK;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            if (character_status == PREVIOUS_IS_NOT_A_BLANK) {
+                putchar(c);
+                character_status = PREVIOUS_IS_A_BLANK;
+            }
+        }
+
+        if (c != ' ') {
+            character_status = PREVIOUS_IS_NOT_A_BLANK;
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
