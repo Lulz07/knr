d95f4159370060805c71623a79b0f76f11737948
Lulz07 <masterluffy2007@gmail.com>
2022-05-24 21:34:31 +0800

    Add solution for exercise 1-7

---
+++ chapter-1/1-7/value-of-eof.c
@@ -0,0 +1,15 @@
+/* Exercise 1-7. (page 17)
+ *
+ * Write a program to print the value of EOF.
+ *
+ * Written by Lulz07.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Value of EOF: %d\n", EOF);
+
+    return 0;
+}
