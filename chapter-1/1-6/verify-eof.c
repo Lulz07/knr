2dae4767fb99d910f20b55daaaeb719f692341a7
Lulz07 <masterluffy2007@gmail.com>
2022-05-24 21:16:51 +0800

    Update the repository's README and add solution for exercise 1-6

---
+++ chapter-1/1-6/verify-eof.c
@@ -0,0 +1,17 @@
+/* Exercise 1-6. (page 17)
+ *
+ * Verify that the expression getchar() != EOF is 0 or 1.
+ *
+ * Written by Lulz07.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int c;
+    c = getchar();
+    printf("c != EOF: %d\n", c != EOF);
+
+    return 0;
+}
