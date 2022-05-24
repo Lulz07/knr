5121c68e086d39e957f3c65b824ee6f864fccfaa
Lulz07 <masterluffy2007@gmail.com>
2022-05-24 22:47:09 +0800

    Made some small changes for replace-blanks.c

--- chapter-1/1-9/replace-blanks.c
+++ chapter-1/1-9/replace-blanks.c
@@ -1,4 +1,5 @@
-/* Exercise 1-9. (page 20)
+/*
+ * Exercise 1-9. (page 20)
  *
  * Write a program to copy its input to its output, replacing
  * each string of one or more blanks by a single blank.
@@ -19,16 +20,17 @@ int main(void)
     while ((c = getchar()) != EOF) {
         if (c == ' ') {
             if (character_status == PREVIOUS_IS_NOT_A_BLANK) {
-                putchar(c);
+                /* change the character status */
                 character_status = PREVIOUS_IS_A_BLANK;
+                putchar(c);
             }
         }
 
         if (c != ' ') {
+            /* reset the character status back */
             character_status = PREVIOUS_IS_NOT_A_BLANK;
             putchar(c);
         }
     }
-
     return 0;
 }
