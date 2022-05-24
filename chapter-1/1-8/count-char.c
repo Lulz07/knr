413105cf6defc040f46dbc8a19d8cf14430ae3f3
Lulz07 <masterluffy2007@gmail.com>
2022-05-24 22:05:59 +0800

    Add solution for exercise 1-8

---
+++ chapter-1/1-8/count-char.c
@@ -0,0 +1,32 @@
+/* Exercise 1-8. (page 20)
+ *
+ * Write a program to count blanks, tabs, and newlines.
+ *
+ * Written by Lulz07.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int c, blanks, tabs, newlines;
+
+    blanks = 0;
+    tabs = 0;
+    newlines = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ')
+            ++blanks;
+        if (c == '\t')
+            ++tabs;
+        if (c == '\n')
+            ++newlines;
+    }
+
+    printf("Blanks: %d\n", blanks);
+    printf("Tabs: %d\n", tabs);
+    printf("Newlines: %d\n", newlines);
+
+    return 0;
+}
