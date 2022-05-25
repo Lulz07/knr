640cc6a73aa1a4dbf5561d48b3e47afa346f0169
Lulz07 <masterluffy2007@gmail.com>
2022-05-25 22:07:46 +0800

    Commenting 1-22/word-per-line.c file

--- chapter-1/1-12/word-per-line.c
+++ chapter-1/1-12/word-per-line.c
@@ -1,3 +1,11 @@
+/*
+ * Exercise 1-12. (page 21)
+ *
+ * Write a program that prints its input one word per line.
+ *
+ * Written by Lulz07.
+*/
+
 #include <stdio.h>
 
 int main(void)
@@ -6,8 +14,10 @@ int main(void)
 
     while ((c = getchar()) != EOF) {
         if (c == ' ' || c == '\n' || c == '\t')
+            /* outside a word */
             printf("\n");
         else
+            /* inside a word */
             putchar(c);
     }
     return 0;
