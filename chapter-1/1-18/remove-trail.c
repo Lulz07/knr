e762f862f1bbe4344a529292e6b0a3d004e22a3c
Lulz07 <masterluffy2007@gmail.com>
2022-06-11 22:14:22 +0800

    Update 1-18/remove-trail.c so that it obeys the Linux kernel coding style

--- chapter-1/1-18/remove-trail.c
+++ chapter-1/1-18/remove-trail.c
@@ -13,55 +13,73 @@
 #define FALSE 0
 #define MAX_CHAR 150
 
-int my_getline(char [], int);
-void remove_trailing_blanks_and_tabs(char [], int);
+int my_getline(char str[], int max_read);
+void remove_trail(char str[], int len);
 
+/* remove trailing blanks and tabs from each line of input */
 int main(void)
 {
     int len;
     char line[MAX_CHAR];
 
     while ((len = my_getline(line, MAX_CHAR)) > 0) {
-        remove_trailing_blanks_and_tabs(line, len);
+        remove_trail(line, len);
         if (len > 1)
             printf("%s", line);
     }
     return 0;
 }
 
-int my_getline(char string[], int max_read)
+/**
+ * my_getline() -- read a line into str, return length.
+ * @str: an array of char to save into
+ * @max_read: maximum buffer to read
+ *
+ * Read a char from stdin and save them into str until a
+ * complete line is formed. The length will be recorded as a
+ * return value.
+ *
+ * Return: The length of str.
+ */
+int my_getline(char str[], int max_read)
 {
     int c, i;
 
-    for (i = 0; i < max_read - 1
-        && (c = getchar()) != EOF
-        && c != '\n';
-        ++i)
-        string[i] = c;
+    for (i = 0; i < max_read - 1 && (c = getchar()) != EOF
+        && c != '\n'; ++i)
+        str[i] = c;
 
     if (c == '\n') {
-        string[i] = c;
+        str[i] = c;
         ++i;
     }
 
-    string[i] = '\0';
+    str[i] = '\0';
     return i;
 }
 
-void remove_trailing_blanks_and_tabs(char string[], int len)
+/**
+ * remove_trail() -- remove trailing blanks and tabs from str.
+ * @str: an array of char to remove from
+ * @len: the length of str
+ *
+ * This function would modify str so that there's no more
+ * trailing blanks and tabs inside it.
+ */
+void remove_trail(char str[], int len)
 {
     int have_trail;
 
     have_trail = FALSE;
     for (len = len - 2; len >= 0
-        && (string[len] == ' ' || string[len] == '\t');
-        --len)
-        have_trail = TRUE;
+        && (str[len] == ' ' || str[len] == '\t'); --len)
+        if (have_trail == FALSE)
+            have_trail = TRUE;
 
     if (have_trail == TRUE) {
         ++len;
-        string[len] = '\n';
+        str[len] = '\n';
         ++len;
-        string[len] = '\0';
+        str[len] = '\0';
     }
 }
